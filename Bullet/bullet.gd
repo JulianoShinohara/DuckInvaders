8009e97624c8e727cbab4d33681588cadead746a
henrque <henrque02@gmail.com>
2023-10-29 15:39:37 -0300

    henrque: add bullet

---
+++ Bullet/bullet.gd
@@ -0,0 +1,10 @@
+class_name Bullet
+extends RigidBody2D
+
+@export var damage: float = 1
+@export var not_target: String = "player"
+
+func _on_body_entered(body: Node) -> void:
+	if not body.is_in_group(not_target):
+		#game_controller.create_explosion("tiny", get_global_position())
+		queue_free()
--- Player/player.gd
+++ Player/player.gd
@@ -1,19 +1,42 @@
+class_name Player
 extends CharacterBody2D
 @onready var _animated_sprite = $AnimatedSprite2D
 
+@export var speed: float = 300
+@export var bullet_speed: float = 400
+@export var fire_delay: float = 0.10
+@export var fire_error: float = 0.08
 
-const SPEED = 300.0
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 
+var _bullet_res: Resource = preload("res://Bullet/bullet.tscn")
+var _input_vector: Vector2
+var _can_fire: bool = true
 
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
 	_animated_sprite.play("default")	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	_input_vector.x = Input.get_axis("ui_left", "ui_right")
+	_input_vector.y = Input.get_axis("ui_up", "ui_down")
+	_input_vector = _input_vector.normalized()
+	
+	if _input_vector != Vector2.ZERO:
+		velocity = _input_vector * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity = _input_vector * 0
+	
 	move_and_slide()
+	
+	if(Input.is_action_pressed("fire")):
+		_fire_bullet()
+
+func _fire_bullet():
+	if not _can_fire:
+		return
+	
+	var bullet: RigidBody2D = _bullet_res.instantiate()
+	bullet.position = $BulletPoint.get_global_position()
+	bullet.apply_impulse(Vector2(0,-bullet_speed), Vector2())
+	get_tree().get_root().add_child(bullet)
+	
+	_can_fire = false
+	await get_tree().create_timer(fire_delay).timeout
+	_can_fire = true	
