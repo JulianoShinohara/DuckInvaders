497cb645ced14379b73b9df8f5b8f02cb1779755
henrque <henrque02@gmail.com>
2023-11-25 21:25:47 -0300

    henrque: score fix

--- Stage1/Score.gd
+++ Stage1/Score.gd
@@ -11,6 +11,10 @@ func update_score(enemy) -> void:
 		game_controller._score += 15
 	if(enemy is Enemy_Atirador): 
 		game_controller._score += 10
+	if(enemy is Enemy_Multiplo): 
+		game_controller._score += 20
 	if(enemy is Boss_fase_1):
 		game_controller._score += 50
+	if(enemy is Boss_fase_2): 
+		game_controller._score += 75
 	text = "Score:" + str(game_controller._score).pad_zeros(5) 
